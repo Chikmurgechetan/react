ea07a5b760c5d5e3d76ee699a65e1f0ad15162ff
Chikmurgechetan <chikmurgechetan@gmail.com>
2023-03-08 02:01:32 +0530

    demo project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from "./componets/Expense";
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Expense.js
@@ -0,0 +1,20 @@
+function ExpenseItem(){
+
+    return (
+
+        <div>
+
+            <div> Expense Items</div>
+
+            <div> Food Rs 10</div>
+
+            <div> Petrol Rs 100</div>
+
+            <div> Movies Rs 200</div>
+
+        </div>
+  )
+
+}
+
+export default ExpenseItem;
